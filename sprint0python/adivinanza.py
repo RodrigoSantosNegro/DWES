9420a052202ab63f886c220c001e1cce5b2d881b
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2023-09-27 09:03:59 +0200

    Completado exercicio 2, añadidas 2 adivinanzas e marcador de puntuación ao final da partida

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,14 +1,53 @@
+puntuacion = 0
+
 print("""Adivina adivinanza...
       ¿Qué es rojo y te puedes comer?
       a) Un ladrillo
       b) Pintura azul
-      c)Un plátano
+      c) Un plátano
       """)
 
 opcion = input('Respuesta (a, b ó c): ')
 
-if(opcion == 'a' or opcion == 'A' or opcion == 'Un ladrillo'):
+if(opcion == 'a' or opcion == 'A' or opcion.lower == 'un ladrillo'):
     print("Respuesta correcta")
+    puntuacion += 10
+
 else:
     print("Nanai, te equivocaste amigo")
+    puntuacion -= 5
     
+print("""Adivina adivinanza...
+      Vuelo de noche, duermo de día, ¿qué soy yo?
+      a) Un búho
+      b) Un universitario promedio
+      c) La luna
+      """)
+    
+opcion = input('Respuesta (a, b ó c): ')
+
+if(opcion == 'c' or opcion == 'C' or opcion.lower == 'la luna'):
+    print("Respuesta correcta")
+    puntuacion += 10
+else:
+    print("La verdad es que todas tienen sentido, pero has fallado :(")
+    puntuacion -= 5
+
+print("""Adivina adivinanza...
+      Adivinanza: Soy un lugar donde puedes encontrar tinta y papel. La gente viene a mí para aprender y leer.
+       ¿Qué soy?
+      a) Biblioteca
+      b) Escuela
+      c) Librería
+      """)
+    
+opcion = input('Respuesta (a, b ó c): ')
+
+if(opcion == 'a' or opcion == 'A' or opcion.lower == 'biblioteca'):
+    print("Respuesta correcta")
+    puntuacion += 10
+else:
+    print("*Sonido de incorrecto* :p")
+    puntuacion -= 5
+
+print("Tu puntuación final ha sido: ", puntuacion)
