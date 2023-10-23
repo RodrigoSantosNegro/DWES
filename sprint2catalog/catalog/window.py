8a8d6ed8f90a5bbe95f026e7963982ad1a6a730e
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2023-10-23 17:28:48 +0200

    Cambios en links de JSON en catalog

--- sprint2catalog/catalog/window.py
+++ sprint2catalog/catalog/window.py
@@ -1,4 +1,9 @@
+from io import BytesIO
 import tkinter as ttk
+
+import requests
+
+from PIL import Image, ImageTk
 from detail_window import DetailWindow
 from cell import Cell
 
@@ -10,21 +15,27 @@ class MainWindow:
         detail_window = DetailWindow(cell.image_tk, cell.title, cell.description)
         detail_window.mainloop()
 
+    def load_image(self, url):
+        response = requests.get(url)
+        image_data = Image.open(BytesIO(response.content))
+        image = ImageTk.PhotoImage(image_data)
+
     def __init__(self,root, json_data):
         root.title("MainWindow")
         self.root = root 
         #guardamos en cells todas las imágenes que queremos mostrar
-        self.cells = [
-            
-        ]
+        self.cells = []
+        data = json_data
+        for x in range(len(data)):
+            name = data[x].get("name")
+            description = data[x].get("description")
+            image_url = data[x].get("image_url")
+            image = self.load_image(image_url)
+
+            self.cells.append(Cell (image, name, description))
+
         #Asignamos a cada imagen una fila distinta (i)
         for i, cell in enumerate(self.cells):
             label = ttk.Label(root, image= cell.image_tk, text=cell.title, compound= ttk.BOTTOM)
             label.grid(row=i,column=0)
             label.bind("<Button-1>", lambda event, cell = cell: self.on_button_click(cell))
-
-
-if __name__ == "__main__":
-    root = ttk.Tk()
-    app = MainWindow(root)
-    root.mainloop()
