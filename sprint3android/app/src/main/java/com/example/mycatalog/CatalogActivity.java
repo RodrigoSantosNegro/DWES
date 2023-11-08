f8c6db6edcbc61895f69e95f063dea4f8d4a858f
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2023-11-08 09:23:54 +0100

    Completada actividad 2. Añadido botón en CatalogActivity para navegar a DetailActivity.

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,7 +2,10 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        // Hacemos referencia al botón creado en activity_catalog
+        Button navigateButton = findViewById(R.id.navigateButton);
+
+        // Agregamos un oyente al botón para manejar eventos de clic
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hacemos que al clicar naveguemos al DetailWindow
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
