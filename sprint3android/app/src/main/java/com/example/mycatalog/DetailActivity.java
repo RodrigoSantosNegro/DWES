a8e1c31690d456129f45ded60d59622bfbd6cdc5
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2023-11-09 09:45:49 +0100

    Completada tarea 4. Añadidos elementos en DetailActivity.

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.mycatalog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ImageView circularImageView = findViewById(R.id.circularImageView);
+        circularImageView.setImageResource(R.drawable.);
+
+        circularImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        circularImageView.setBackground(getResources().getDrawable(R.drawable.circular_image_border));
     }
 }
