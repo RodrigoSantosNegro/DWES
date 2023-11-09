33c0cec4671f6834df8e16fb88c9e0b56325556a
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2023-11-09 11:24:32 +0100

    Completada tarea 4. Añadido activity_detail.xml(land) para ver formato de móvil girado.

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -11,10 +11,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        ImageView circularImageView = findViewById(R.id.circularImageView);
-        circularImageView.setImageResource(R.drawable.);
 
-        circularImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        circularImageView.setBackground(getResources().getDrawable(R.drawable.circular_image_border));
     }
 }
