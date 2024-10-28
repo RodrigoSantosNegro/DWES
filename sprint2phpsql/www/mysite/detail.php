30932991d1d8f2ac3eeccadb6c02026107cf47b6
Rodrigo Santos <jsantosn23@fpcoruna.afundacion.org>
2024-10-28 10:00:45 +0100

    Ejercicio 3

---
+++ sprint2phpsql/www/mysite/detail.php
@@ -0,0 +1,46 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+?>
+
+<html>
+<body>
+<?php
+if (!isset($_GET['id'])) {
+    die('No se ha especificado un libro');
+}
+
+$libro_id = $_GET['id'];
+
+$query = "SELECT * FROM tLibros WHERE id = $libro_id";
+$result = mysqli_query($db, $query) or die('Error en la consulta del libro');
+$libro = mysqli_fetch_array($result);
+
+if (!$libro) {
+    die('Libro no encontrado');
+}
+
+// Mostramos detalles del libro
+echo '<h1>'.$libro['nombre'].'</h1>';
+echo '<img src="'.$libro['url_imagen'].'" alt="Imagen de '.$libro['nombre'].'" style="width:200px;height:300px;"><br>';
+echo '<p>Autor: '.$libro['autor'].'</p>';
+echo '<p>Año de publicación: '.$libro['año_publicacion'].'</p>';
+?>
+
+<h3>Comentarios:</h3>
+<ul>
+<?php
+$query2 = "SELECT * FROM tComentarios WHERE libro_id = $libro_id";
+$result2 = mysqli_query($db, $query2) or die('Error en la consulta de comentarios');
+
+
+while ($comentario = mysqli_fetch_array($result2)) {
+    echo '<li>'.$comentario['comentario'].'</li>';
+}
+?>
+</ul>
+
+<?php
+mysqli_close($db);
+?>
+</body>
+</html>
